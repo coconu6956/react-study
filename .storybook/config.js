4e1c4f59c7dcf3b52dfefa57f593749fcc6ca01e
chaeeun <btd.chaeeun@gmail.com>
2020-12-21 01:57:00 +0900

    가상코인 거래소 만들기

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 import interopRequireDefault from 'babel-runtime/helpers/interopRequireDefault';
-
+import '../src/doit-ui/app.css';
 function loadStories() {
     const context = require.context('../src/stories', true, /Story\.js$/);
     context.keys().forEach((srcFile) => {
---
+++ src/doit-ui/inlineList.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, css, withStylesPropTypes } from './withStyles';
+import { unit } from './Theme';
+
+class inlineList extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default inlineList;
